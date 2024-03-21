093e36f0a2ef3f5dea085bf3a889c110754bdfdc
Boris Goldzamd <125725241+BorisGoldzamd@users.noreply.github.com>
2024-03-21 10:37:08 +0200

    carrousel series

---
+++ frontend/src/components/Carousel.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import '../assets/carousel.css';
+
+const Carousel = ({ items }) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    cssEase: 'ease-in-out', // Animación personalizada
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+
+  return (
+    <div className="carousel-container">
+      <Slider {...settings}>
+        {items.map((item, index) => (
+          <div key={index} className="carousel-item">
+            <iframe
+              width="100%"
+              height="315"
+              src={item.videoUrl}
+              title={item.title}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default Carousel;
--- frontend/src/components/Series.jsx
+++ frontend/src/components/Series.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import Carousel from './Carousel';
 
 const Series = () => {
-    return (
-        <div>
-            <h1>Series</h1>
-            <p>Welcome to the Series page!</p>
-            {/* Add more content as needed */}
-        </div>
-    );
-}
+  // Aquí recuperas los datos de las series desde tu base de datos o cualquier otra fuente
+  const seriesData = [
+    { videoUrl: 'https://www.youtube.com/embed/EySdVK0NK1Y?si=W7iiW9gE7ABK1EgN', title: 'Serie 1' },
+    { videoUrl: 'https://www.youtube.com/embed/_inKs4eeHiI?si=g1HhqbEuuIJG9gJG', title: 'Serie 2' }
+    // Agrega más datos de series según sea necesario
+  ];
+
+  return (
+    <div>
+      <h2>Series</h2>
+      <Carousel items={seriesData} />
+    </div>
+  );
+};
 
 export default Series;
