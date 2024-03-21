9c828c081c08b1768d9f076846fa9857b9f1cdf4
Boris Goldzamd <125725241+BorisGoldzamd@users.noreply.github.com>
2024-03-21 09:07:56 +0200

    finished navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faCog, faFilm, faTv } from '@fortawesome/free-solid-svg-icons';
+import '../assets/navbar.css';
 
 const Navbar = () => {
     return (
-        <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/movies">Movies</Link></li>
-                <li><Link to="/series">Series</Link></li>
-                <li><Link to="/settings">Settings</Link></li>
+        <nav className="navbar-container">
+            <ul className="navbar-list">
+                <li className="navbar-item">
+                    <Link to="/" className="navbar-link">
+                        <FontAwesomeIcon icon={faHome} className="navbar-icon" />
+                        Home
+                    </Link>
+                </li>
+                <li className="navbar-item">
+                    <Link to="/movies" className="navbar-link">
+                        <FontAwesomeIcon icon={faFilm} className="navbar-icon" />
+                        Movies
+                    </Link>
+                </li>
+                <li className="navbar-item">
+                    <Link to="/series" className="navbar-link">
+                        <FontAwesomeIcon icon={faTv} className="navbar-icon" />
+                        Series
+                    </Link>
+                </li>
+                <li className="navbar-item-settings">
+                    <Link to="/settings" className="navbar-link">
+                        <FontAwesomeIcon icon={faCog} className="navbar-icon" />
+                        <span className="navbar-text">Settings</span>
+                    </Link>
+                </li>
             </ul>
         </nav>
     );
